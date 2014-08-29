20a885698cec8c0211d3326c7c438130e82b7d16
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-08-29 13:54:42 +0600

    Remove one item from menu

--- pages/index/menu.bemjson.js
+++ pages/index/menu.bemjson.js
@@ -2,7 +2,6 @@ module.exports = {
     block: 'menu',
     content: [
         { elem: 'heading', content: 'Get BEM' },
-        { elem: 'item', content: 'Quick start', url: 'https://github.com/matmuchrapna/gulp-bem-stub' },
         { elem: 'item', content: 'Awesome BEM', url: 'https://github.com/floatdrop/awesome-bem' },
         { elem: 'item', content: 'BEM.info', url: 'https://bem.info/' }
     ]
