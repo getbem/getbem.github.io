f66312f3d3a6a988ee913fff3207be2c4b3ad2c0
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-09-05 23:06:44 +0600

    Use gulp-watch for watching

--- gulpfile.ext.js
+++ gulpfile.ext.js
@@ -1,12 +1,16 @@
 var gulp = require('gulp');
 var buildBranch = require('buildbranch');
+var watch = require('gulp-watch');
 
 gulp.task('watch', ['build'], function() {
-    return gulp.watch([
+    return watch([
         '{blocks,pages}/**/*.deps.js',
         '{blocks,pages}/**/*.css',
-        '{blocks,pages}/**/*.jade'
-    ], ['build']);
+        '{blocks,pages}/**/*.jade',
+        '{blocks,pages}/**/*.md'
+    ], function () {
+        gulp.start('build');
+    });
 });
 
 gulp.task('gh', ['build'], function(done) {
