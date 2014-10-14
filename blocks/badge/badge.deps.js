1784e68c65c9b54bca6717fc816409a857a0db3d
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-10-14 13:06:54 +0600

    Rewrite gulpfile from my dreams

---
+++ blocks/badge/badge.deps.js
@@ -0,0 +1,5 @@
+module.exports = {
+    require: [
+        { block: 'colors' }
+    ]
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,73 +1,68 @@
 var gulp = require('gulp');
 var bem = require('gulp-bem');
-var plumber = require('gulp-plumber');
 var concat = require('gulp-concat');
 var del = require('del');
 var jade = require('gulp-jade');
 var sass = require('gulp-sass');
 var pack = require('gulp-bem-pack');
-var save = require('save-stream');
-var glue = require('glue-streams');
-var through = require('through2');
-var join = require('path').join;
 var autoprefixer = require('gulp-autoprefixer');
 var uglify = require('gulp-uglify');
-
-function getCssFiles(bemObject) {
-    return gulp.src(join(bemObject.path, bemObject.id + '.css'));
-}
+var buildBranch = require('buildbranch');
 
 var levels = [
     'js',
     'libs/bootstrap/levels/*',
     'base',
-    'blocks'
+    'blocks',
+    'pages'
 ];
 
+var tree;
+gulp.task('tree', function () {
+    tree = bem.objects(levels).pipe(bem.deps()).pipe(bem.tree());
+});
+
 gulp.task('js', ['clean'], function () {
-    return glue.obj(bem.objects(levels), bem.objects('pages'))
-        .pipe(bem.src('{bem}.js'))
-        .pipe(pack('index.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('./dist'));
+    function buildJs(page) {
+        return tree.deps('pages/' + page.id)
+            .pipe(bem.src('{bem}.js'))
+            .pipe(pack(page.id + '.js'))
+            .pipe(uglify())
+            .pipe(gulp.dest('./dist'));
+    }
+
+    return bem.objects('pages').map(buildJs);
 });
 
 gulp.task('css', ['clean'], function () {
-    var levelsCss = bem.objects(levels)
-        .pipe(bem.src('{bem}.{scss,css}'))
-        .pipe(save());
+    function buildCss(page) {
+        return tree.deps('pages/' + page.id)
+            .pipe(bem.src('{bem}.{scss,css}'))
+            .pipe(concat(page.id + '.css'))
+            .pipe(sass())
+            .pipe(autoprefixer({
+                browsers: ['last 2 versions'],
+                cascade: false
+            }))
+            .pipe(gulp.dest('./dist'));
+    }
 
-    return bem.objects('pages')
-        .pipe(through.obj(function (obj, enc, cb) {
-            return glue.obj(levelsCss.load(), getCssFiles(obj))
-                .pipe(concat(obj.id + '.css'))
-                .pipe(sass())
-                .pipe(autoprefixer({
-                    browsers: ['last 2 versions'],
-                    cascade: false
-                }))
-                .pipe(gulp.dest('./dist'))
-                .on('error', cb)
-                .on('end', cb);
-        }));
+    return bem.objects('pages').map(buildCss);
 });
 
 gulp.task('html', ['clean'], function () {
-    var mixins = bem.objects(levels)
-        .pipe(bem.src('{bem}.jade'))
-        .pipe(save());
+    function buildHtml(page) {
+        return tree.deps('pages/' + page.id)
+            .pipe(bem.src('{bem}.jade'))
+            .pipe(concat({
+                path: page.path + '/' + page.id + '.jade',
+                base: page.path
+            }))
+            .pipe(jade({pretty: true}))
+            .pipe(gulp.dest('./dist'));
+    }
 
-    return bem.objects('pages')
-        .pipe(bem.src('{bem}.jade'))
-        .pipe(through.obj(function (page, enc, cb) {
-            return glue.obj(mixins.load(), page)
-                .pipe(concat(page))
-                .pipe(plumber())
-                .pipe(jade({pretty: true}))
-                .pipe(gulp.dest('./dist'))
-                .on('error', cb)
-                .on('end', cb);
-        }));
+    return bem.objects('pages').map(buildHtml);
 });
 
 gulp.task('cname', ['clean'], function () {
@@ -78,11 +73,21 @@ gulp.task('assets', ['clean'], function () {
     return gulp.src('assets/**').pipe(gulp.dest('dist'));
 });
 
-gulp.task('clean', function (cb) {
+gulp.task('clean', ['tree'], function (cb) {
     del(['./dist'], cb);
 });
 
 gulp.task('build', ['clean', 'html', 'css', 'js', 'assets', 'cname']);
 
-/* Some external tasks */
-require('./gulpfile.ext.js');
+gulp.task('gh', ['build'], function(done) {
+    buildBranch({ folder: 'dist', ignore: ['libs'] }, done);
+});
+
+var watch = require('gulp-watch');
+gulp.task('watch', ['build'], function() {
+    return watch(levels.map(function (l) { return l + '/**/*.{js,css,jade}'; }), function () {
+        gulp.start('build');
+    });
+});
+
+gulp.task('default', ['watch']);
