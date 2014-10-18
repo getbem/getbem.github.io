9f4a009387f7dcff75283b7376823973766db3fe
Vladimir Starkov <vstarkov@yandex-team.ru>
2014-10-19 01:35:19 +0600

    refactor(tw-btn): small refactoring

---
+++ levels/blocks/tw-btn/tw-btn.js
@@ -0,0 +1,1 @@
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
