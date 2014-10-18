1e7bd0588ae1348d8c2f1584b1bc5a6e7eaf59c8
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-10-19 02:51:41 +0600

    fix(tests): page urls are changed

--- test/index.js
+++ test/index.js
@@ -1,33 +1,10 @@
-/* global describe, it */
+/* global it */
 
 var Nightmare = require('nightmare');
 var join = require('path').join;
 
-describe('index page', function () {
-    it('should have global jQuery', function (done) {
-        new Nightmare()
-            .goto(join(__dirname, '../dist/index.html'))
-            .evaluate(function() {
-                /* global jQuery */
-                return jQuery;
-            }, function (res) {
-                if (res === null) {
-                    return done('jQuery is not found on page');
-                }
-                done();
-            })
-            .run(function (err) {
-                if (err) { done(err); }
-            });
-    });
-
-    it('should have link to introduction', function (done) {
-        new Nightmare()
-            .goto(join(__dirname, '../dist/index.html'))
-            .click('.nav__link[href="introduction.html"]')
-            .run(function (err) {
-                if (err) { return done(err); }
-                done();
-            });
-    });
+it('should have index page', function (done) {
+    new Nightmare()
+        .goto(join(__dirname, '../dist/index.html'))
+        .run(done);
 });
