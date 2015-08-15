48e683cc0c3f04aea0d8709661eee3429f74f1b3
Vsevolod Strukchinsky <floatdrop@gmail.com>
2015-08-15 14:02:49 +0500

    Tabs layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,34 @@
 import React, {Component} from 'react';
-require('./app.css');
 
 export default class App extends Component {
 	render() {
 		return (
-			<div className='layout-transparent mdl-layout mdl-js-layout'>
-				<header className='mdl-layout__header mdl-layout__header--transparent'>
-					<div className='mdl-layout__header-row'>
-						<span className='mdl-layout-title'>Title</span>
-						<div className='mdl-layout-spacer'></div>
-						<nav className='mdl-navigation'>
-							<a className='mdl-navigation__link' href=''>Introduction</a>
-							<a className='mdl-navigation__link' href=''>Naming</a>
-							<a className='mdl-navigation__link' href=''>FAQ</a>
-						</nav>
-					</div>
-				</header>
-				<div className='mdl-layout__drawer'>
-					<span className='mdl-layout-title'>Title</span>
-					<nav className='mdl-navigation'>
-						<a className='mdl-navigation__link' href=''>Introduction</a>
-						<a className='mdl-navigation__link' href=''>Naming</a>
-						<a className='mdl-navigation__link' href=''>FAQ</a>
-					</nav>
-				</div>
-				<main className='mdl-layout__content' style={{textAlign: 'center'}}>
-					<img width='250' height='300' style={{display: 'inline-block'}} src='/assets/b_.svg'></img>
-				</main>
+			<div className="layout-transparent mdl-layout mdl-js-layout mdl-layout--fixed-header">
+			  <header className="mdl-layout__header mdl-layout__header--transparent">
+			    <div className="mdl-layout__header-row">
+			      <span className="mdl-layout-title">Get BEM</span>
+			    </div>
+
+			    <div className="mdl-layout__tab-bar mdl-js-ripple-effect">
+			      <a href="#scroll-tab-1" className="mdl-layout__tab">Introduction</a>
+			      <a href="#scroll-tab-2" className="mdl-layout__tab">Naming</a>
+			      <a href="#scroll-tab-3" className="mdl-layout__tab">FAQ</a>
+			    </div>
+			  </header>
+			  <div className="mdl-layout__drawer">
+			    <span className="mdl-layout-title">Get BEM</span>
+			  </div>
+			  <main className="mdl-layout__content">
+			    <section className="mdl-layout__tab-panel" id="scroll-tab-1">
+			      <div className="page-content">Introduction</div>
+			    </section>
+			    <section className="mdl-layout__tab-panel" id="scroll-tab-2">
+			      <div className="page-content">Naming</div>
+			    </section>
+			    <section className="mdl-layout__tab-panel" id="scroll-tab-3">
+			      <div className="page-content">FAQ</div>
+			    </section>
+			  </main>
 			</div>
 		);
 	}
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import App from './App';
-
 require('material-design-lite/material.css');
 require('material-design-lite/material');
+
+import React from 'react';
+import App from './App';
+require('./app.css');
 
 React.render(<App />, document.getElementById('root'));
