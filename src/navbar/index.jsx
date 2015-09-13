ee172b0190b495382e2e7f5146f7283cf1d78dda
Vsevolod Strukchinsky <floatdrop@gmail.com>
2015-09-13 11:04:35 +0500

    More mobile friendly

--- src/navbar/index.jsx
+++ src/navbar/index.jsx
@@ -6,13 +6,12 @@ export default class Navbar extends Component {
 		return (
 			<div className='navbar'>
 				<div style={{marginLeft: 'auto', marginRight: 'auto', maxWidth: 1035}}>
-					<a className='navbar__title' href='/'>Get BEM</a>
 					<div className='navbar__menu'>
 						<ul>
+							<li className='navbar__item--title'><a href='/'>Get BEM</a></li>
 							<li className={this.props.active === 'introduction' ? 'navbar__item--active' : ''}><a href='/introduction/'>Introduction</a></li>
 							<li className={this.props.active === 'naming' ? 'navbar__item--active' : ''}><a href='/naming/'>Naming</a></li>
 							<li className={this.props.active === 'faq' ? 'navbar__item--active' : ''}><a href='/faq/'>FAQ</a></li>
-							<li><a href='http://github.com/getbem/getbem.com'>GitHub</a></li>
 						</ul>
 					</div>
 				</div>
