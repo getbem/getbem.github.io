7b236dc2286dac62a4eede087be84ed700382a72
Vsevolod Strukchinsky <floatdrop@gmail.com>
2016-05-15 01:25:50 +0500

    add missing licenses

--- src/blocks/footer.js
+++ src/blocks/footer.js
@@ -7,7 +7,7 @@ export default () => (
 		<p>Brought to you by <a href="https://github.com/floatdrop">@floatdrop</a> and <a href="https://twitter.com/iamstarkov">@iamstarkov</a>.
 		<br/>
 		Maintained by the <a href="https://github.com/orgs/getbem/people">core team</a> with the help of our <a href="https://github.com/getbem/getbem.com/graphs/contributors">contributors</a>.<br/>
-		Code licensed under <a href="https://github.com/getbem/getbem.com/blob/master/LICENSE.md">MIT</a>, documentation under <a href="https://github.com/getbem/getbem.com/blob/master/levels/pages/LICENSE.md">CC BY 3.0</a>.</p>
+		Code licensed under <a href="https://github.com/getbem/getbem.com/blob/master/LICENSE.md">MIT</a>, documentation under <a href="https://github.com/getbem/getbem.com/blob/master/src/markdown/LICENSE.md">CC BY 3.0</a>.</p>
 		<br/>
 		<ul className="footer__links">
 			<li><a href="https://twitter.com/getbem">Twitter</a></li>
