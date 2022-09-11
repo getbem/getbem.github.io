acd77c1e1b9d1d7e6badc1d094013a60afc657c0
Vsevolod Strukchinsky <floatdrop@users.noreply.github.com>
2022-09-11 10:06:37 +0300

    Fix hidden star counter bug (#166) (#167)
    * Fix failed view load when url has an ID
    
    This fix resolves the dead link bug described in issue https://github.com/getbem/getbem.com/issues/147
    
    Currently, the url if statement is looking for a view that does not exist. This is because it is checking against the url post '/'.
    i.e. 'faq/#custom-tags-for-blocks' is interpreted as a page rather than the page '/faq' AND an anchor '#custom-tags-for-blocks'.
    
    To solve this, we check to see if the URL contains an ID. If true, we remove the ID and load the appropriate view.
    
    * Fix hidden star counter bug (#166)
    
    * Fix star counter overflow
    
    Co-authored-by: Jonathan Milanovic <j@sonecoin.com>

--- src/blocks/footer.js
+++ src/blocks/footer.js
@@ -1,7 +1,7 @@
 export default () => (
 	<div className="footer">
 		<div className="widgets">
-			<iframe src="https://ghbtns.com/github-btn.html?user=getbem&repo=getbem.com&type=star&count=true&size=large" frameBorder="0" scrolling="0" width="130px" height="30px"></iframe>
+			<iframe src="https://ghbtns.com/github-btn.html?user=getbem&repo=getbem.com&type=star&count=true&size=large" frameBorder="0" scrolling="0" width="140px" height="30px"></iframe>
 		</div>
 		<br/>
 		<p>Brought to you by <a href="https://github.com/floatdrop">@floatdrop</a> and <a href="https://twitter.com/iamstarkov">@iamstarkov</a>.
