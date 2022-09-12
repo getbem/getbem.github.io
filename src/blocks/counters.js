68db238f53cfda494d0edd79d19d28dff1b193f6
Vsevolod Strukchinsky <floatdrop@users.noreply.github.com>
2022-09-12 09:51:27 +0300

    Remove gauges

--- src/blocks/counters.js
+++ src/blocks/counters.js
@@ -7,8 +7,7 @@ ga('send', 'pageview');
 </script>
 <script async src='//www.google-analytics.com/analytics.js'></script>
 <!-- End Google Analytics -->`;
-const gauges = '<script type=\'text/javascript\'>var _gauges = _gauges || [];(function() {var t = document.createElement(\'script\'); t.type  = \'text/javascript\'; t.async = true; t.id    = \'gauges-tracker\'; t.setAttribute(\'data-site-id\', \'54070a998bfdf7145100b613\'); t.setAttribute(\'data-track-path\', \'https://track.gaug.es/track.gif\'); t.src = \'https://track.gaug.es/track.js\'; var s = document.getElementsByTagName(\'script\')[0]; s.parentNode.insertBefore(t, s); })();</script>';
 
 export default () => (
-	<div innerHTML={googleAnalitycs + gauges}></div>
+	<div innerHTML={googleAnalitycs}></div>
 );
