be2eabc63a8ac24f6a4a9c2cbbe00e9699791d3e
Vsevolod Strukchinsky <floatdrop@users.noreply.github.com>
2022-09-15 18:39:30 +0000

    Move contant of introduction to mdx (part 1)

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,18 @@ import solidJs from "@astrojs/solid-js";
 import lightningcss from "vite-plugin-lightningcss";
 import image from "@astrojs/image";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://getbem.com",
-  integrations: [tailwind(), solidJs(), image()],
+  integrations: [tailwind(), solidJs(), image(), mdx()],
   vite: {
     plugins: [lightningcss({
       browserslist: "last 2 versions"
     })],
     ssr: {
-      external: ["svgo"],
-    },
+      external: ["svgo"]
+    }
   }
 });
