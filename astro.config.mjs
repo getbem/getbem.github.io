e1d8bee799e3a8481f73444d75d83be9f5ade517
Vsevolod Strukchinsky <floatdrop@users.noreply.github.com>
2022-09-16 16:54:01 +0000

    Add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,9 @@ import solidJs from "@astrojs/solid-js";
 import lightningcss from "vite-plugin-lightningcss";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
-
 import partytown from "@astrojs/partytown";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,9 +14,9 @@ export default defineConfig({
   integrations: [tailwind(), solidJs(), image(), mdx(), partytown({
     // Adds dataLayer.push as a forwarding-event.
     config: {
-      forward: ["dataLayer.push"],
-    },
-  })],
+      forward: ["dataLayer.push"]
+    }
+  }), sitemap()],
   vite: {
     plugins: [lightningcss({
       browserslist: "last 2 versions"
