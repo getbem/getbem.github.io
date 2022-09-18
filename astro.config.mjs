87ef691e4ae0667db04b917bf171cebe2f57c5cc
Vsevolod Strukchinsky <floatdrop@gmail.com>
2022-09-18 13:40:44 +0500

    Remove partytown (cors problems)

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,19 +4,13 @@ import solidJs from "@astrojs/solid-js";
 import lightningcss from "vite-plugin-lightningcss";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
-import partytown from "@astrojs/partytown";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://getbem.com",
-  integrations: [tailwind(), solidJs(), image(), mdx(), partytown({
-    // Adds dataLayer.push as a forwarding-event.
-    config: {
-      forward: ["dataLayer.push"]
-    }
-  }), sitemap()],
+  integrations: [tailwind(), solidJs(), image(), mdx(), sitemap()],
   vite: {
     plugins: [lightningcss({
       browserslist: "last 2 versions"
