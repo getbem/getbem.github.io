554e62ccbc1ff832bd597d43dc1838fe46d640a6
Vsevolod Strukchinsky <floatdrop@users.noreply.github.com>
2023-04-20 17:17:24 +0500

    Revert "add anchor links to headings (#303)"
    This reverts commit 5d79f391a6e572b6dffde6a777f90265c7303562.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', './astro.config.mjs'],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
